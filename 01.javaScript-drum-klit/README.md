9f26f55659fd0e4b30c9c16e84fa7d2b0d46b409
米老朱 <laozhu.me@gmail.com>
2018-10-14 03:00:03 +0800

    Complete javaScript drum kit chanllege

---
+++ 01.javaScript-drum-klit/README.md
@@ -0,0 +1,17 @@
+## JavaScript Drum Kit
+
+> Melaozhu's version
+
+### Features
+
+- [x] Responsive layout
+- [x] Support click event
+- [x] Support long key press
+- [x] Stop sound when 500ms later
+- [x] Stop sound when sound ended
+
+### Caveats
+
+1. DOMException: The play() request was interrupted
+
+<https://developers.google.com/web/updates/2017/06/play-request-was-interrupted>
--- 01.javaScript-drum-klit/js/main.js
+++ 01.javaScript-drum-klit/js/main.js
@@ -1,1 +1,66 @@
-console.log('hello world');
+const playSound = ({ sound, success }) => {
+  /**
+   * Play specific drum sound
+   * Make success callback after 500ms or sound ended.
+   *
+   * @param {Node} sound (Selected audio node)
+   * @param {Function} sucess (Callback after palying audio)
+   * @return {Null}
+   */
+
+  const playingSound = sound;
+  const playPromise = playingSound.play();
+
+  // Stop sound function with success callback.
+  const stopSound = () => {
+    playingSound.pause();
+    playingSound.currentTime = 0;
+    success && success();
+  };
+
+  if (playPromise !== undefined) {
+    playingSound.currentTime = 0; // Reset audio.
+    playPromise
+      .then(() => {
+        setTimeout(stopSound, 500); // When 500ms later stop sound.
+        sound.addEventListener('ended', stopSound); // When audio ended stop sound.
+      })
+      .catch(error => console.log(error));
+  }
+};
+
+// When key down play sound.
+document.addEventListener('keydown', keyDownEvent => {
+  const code = keyDownEvent.keyCode;
+  const selector = `data-key='${code}'`;
+  const key = document.querySelector(`.key[${selector}]`);
+  const sound = document.querySelector(`audio[${selector}]`);
+  if (!key || !sound) return;
+  key.classList.add('playing');
+  playSound({ sound });
+
+  // When key up remove playing class name from key.
+  document.addEventListener('keyup', keyUpEvent => {
+    if (keyUpEvent.keyCode === code) {
+      key.classList.remove('playing');
+    }
+  });
+});
+
+// When key clicked play sound.
+const keys = document.querySelectorAll('.key');
+keys &&
+  Array.from(keys).map(key => {
+    key.addEventListener('click', () => {
+      const code = key.dataset.key;
+      const selector = `audio[data-key='${code}']`;
+      const sound = document.querySelector(selector);
+      if (!key || !sound) return;
+      key.classList.add('playing');
+      playSound({
+        sound,
+        success: () => key.classList.remove('playing'),
+      });
+    });
+    return key;
+  });
