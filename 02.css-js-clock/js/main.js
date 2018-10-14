e429a58d33e53558748de2936b8fbd49ec677452
米老朱 <laozhu.me@gmail.com>
2018-10-14 23:56:41 +0800

    Complete CSS + JS Clock chanllege

--- 02.css-js-clock/js/main.js
+++ 02.css-js-clock/js/main.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const setTime = () => {
+  /**
+   * Get time and set degree of clock hands.
+   *
+   * @param {Null}
+   * @return {Null}
+   */
+
+  const now = new Date();
+
+  const hour = now.getHours();
+  const hourHand = document.querySelector('.hour-hand');
+  hourHand.style.transform = `rotate(${hour * 15}deg)`;
+
+  const minute = now.getMinutes();
+  const minuteHand = document.querySelector('.min-hand');
+  minuteHand.style.transform = `rotate(${minute * 6}deg)`;
+
+  const second = now.getSeconds();
+  const secondHand = document.querySelector('.second-hand');
+  secondHand.style.transform = `rotate(${second * 6}deg)`;
+};
+
+setTime(); // Set time once before setInterval
+setInterval(setTime, 1000);
